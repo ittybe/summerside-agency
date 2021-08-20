922b977ef7a055a3302070a09839615afc91e8db
michael <ittybemain@gmail.com>
2021-08-20 22:02:33 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader", "postcss-loader"]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: "index.html",
+            inject: "head",
+            scriptLoading: "defer" // speed up page time loading 
+        })
+    ],
+    mode: "development"
+};
+
+module.exports = config;
