7014841406b2d495df7a7cfaf5d5c3dc223892f0
michael <ittybemain@gmail.com>
2021-08-23 21:10:50 +0300

    set up font awesome

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import css from "./styles.css"
 import vars from "./variables.css"
 import cssmobile from "./mobile-styles.css"
+
+import brands from "../node_modules/@fortawesome/fontawesome-free/js/brands"
+import fontawesome from "../node_modules/@fortawesome/fontawesome-free/js/fontawesome"
+import solid from "../node_modules/@fortawesome/fontawesome-free/js/solid"
